3058116acd385c9018fd8026f32e04497412a8d4
Adarsh-Liju <adarsh.liju.abraham@gmail.com>
2021-10-31 23:33:36 +0530

    Update CSS file

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   });
   const handleSubmit=(event)=>{
     event.preventDefault();
-    
+
     setSubmitted(true);
   }
   const [submitted,setSubmitted]=useState(false)
@@ -21,7 +21,7 @@ function App() {
   const handleEmailInputChange=(event)=>{
     setValues({...values,email:event.target.value})
   }
-    
+
     return(
       <div className="form-container">
         <form className="reg-form" onSubmit={handleSubmit}>
@@ -31,21 +31,21 @@ function App() {
           value={values.firstName}
           className="field"
           placeholder="First Name"
-          name="firstName"/>
+          name="firstName"/><br/>
           {submitted && !values.firstName ?<span>Please enter a first name</span>:null}
           <input
           onChange={handleLastNameInputChange}
           value={values.lastName}
           className="field"
           placeholder="Last Name"
-          name="lastName"/>
+          name="lastName"/><br/>
           {submitted && !values.lastName ?<span>Please enter a last name</span>:null}
           <input
           onChange={handleEmailInputChange}
           value={values.email}
           className="field"
           placeholder="Email"
-          name="email"/>
+          name="email"/><br/>
           {submitted && !values.email ?<span>Please enter an email address</span>:null}
           <button type="submit">Submit</button>
         </form>
