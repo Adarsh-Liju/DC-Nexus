b40bea89958cc6073171a21b11c1e1aca7139f3a
Adarsh Liju <adarsh.liju.abraham@gmail.com>
2021-11-03 17:33:15 +0530

    ...
    Signed-off-by: Adarsh Liju <adarsh.liju.abraham@gmail.com>

---
+++ build/precache-manifest.f47d6517c87f32231cd8fb215ecb4a13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "726a448aebe03e4e6ee47a915b9d2700",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9f106f84ca7b9458ada",
+    "url": "/static/css/main.30317e7a.chunk.css"
+  },
+  {
+    "revision": "26aac6d835c3810b9285",
+    "url": "/static/js/2.70e48ced.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.70e48ced.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9f106f84ca7b9458ada",
+    "url": "/static/js/main.a9676965.chunk.js"
+  },
+  {
+    "revision": "349aed911c2b92424ea6",
+    "url": "/static/js/runtime-main.e7e84042.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,72 +1,101 @@
 import React from "react";
-import './App.css'
-
+import "./App.css";
 function App() {
-  var values={
-    firstName:"",
-    lastName:"",
-    email:"",
+  var values = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    tel: 0,
+    query: "",
   };
-  const handleSubmit=(event)=>{
-    alert("Thanks for the feedback!!")
+  function handleSubmit(event){
     event.preventDefault();
-    console.log(values)
-  }
-    return(
-      <div class="background">
-           
-      <div class="container">
-        <div class="screen">
-          <div class="screen-header">
-            <div class="screen-header-left">
-              <div class="screen-header-button close"></div>
-              <div class="screen-header-button maximize"></div>
-              <div class="screen-header-button minimize"></div>
-            </div>
-            <div class="screen-header-right">
-              <div class="screen-header-ellipsis"></div>
-              <div class="screen-header-ellipsis"></div>
-              <div class="screen-header-ellipsis"></div>
-            </div>
-          </div>
-          <div class="screen-body">
-            <div class="screen-body-item left">
-              <div class="app-title">
-                <span>CONTACT</span>
-                <span>US</span>
+    values.firstName = document.getElementById("firstName").value;
+    values.lastName = document.getElementById("lastName").value;
+    values.email = document.getElementById("email").value;
+    values.tel = document.getElementById("tel").value;
+    values.query = document.getElementById("query").value;
+    console.log(values);
+  };
+  return (
+    <div className="background">
+      <form onSubmit={handleSubmit}>
+        <div className="container">
+          <div className="screen">
+            <div className="screen-header">
+              <div className="screen-header-left">
+                <div className="screen-header-button close"></div>
+                <div className="screen-header-button maximize"></div>
+                <div className="screen-header-button minimize"></div>
               </div>
-              <div class="app-contact">CONTACT INFO :+91 8088229177</div>
+              <div className="screen-header-right">
+                <div className="screen-header-ellipsis"></div>
+                <div className="screen-header-ellipsis"></div>
+                <div className="screen-header-ellipsis"></div>
+              </div>
             </div>
-            <div class="screen-body-item">
-              <div class="app-form">
-                <div class="app-form-group">
-                  <input class="app-form-control" placeholder="FIRST NAME" type="text" />
+            <div className="screen-body">
+              <div className="screen-body-item left">
+                <div className="app-title">
+                  <span>CONTACT</span>
+                  <span>US</span>
                 </div>
-                <div class="app-form-group">
-                  <input class="app-form-control" placeholder="LAST NAME" type="text" />
-                </div>
-                <div class="app-form-group">
-                  <input class="app-form-control" placeholder="EMAIL" type="email" />
-                </div>
-                <div class="app-form-group">
-                  <input class="app-form-control" placeholder="CONTACT NO" type="tel"/>
-                </div>
-                <div class="app-form-group message">
-                  <input class="app-form-control" placeholder="YOUR QUERY"/>
-                </div>
-                <div class="app-form-group buttons">
-                  <button class="app-form-button">CANCEL</button>
-                  <button class="app-form-button" onSubmit={handleSubmit}>SEND</button>
+                <div className="app-contact">CONTACT INFO :+91 8088229177</div>
+              </div>
+              <div className="screen-body-item">
+                <div className="app-form">
+                  <div className="app-form-group">
+                    <input
+                      className="app-form-control"
+                      placeholder="FIRST NAME"
+                      type="text"
+                      id="firstName"
+                    />
+                  </div>
+                  <div className="app-form-group">
+                    <input
+                      className="app-form-control"
+                      placeholder="LAST NAME"
+                      type="text"
+                      id="lastName"
+                    />
+                  </div>
+                  <div className="app-form-group">
+                    <input
+                      className="app-form-control"
+                      placeholder="EMAIL"
+                      type="email"
+                      id="email"
+                    />
+                  </div>
+                  <div className="app-form-group">
+                    <input
+                      className="app-form-control"
+                      placeholder="CONTACT NO"
+                      type="tel"
+                      id="tel"
+                    />
+                  </div>
+                  <div className="app-form-group message">
+                    <input
+                      className="app-form-control"
+                      placeholder="YOUR QUERY"
+                      id="query"
+                    />
+                  </div>
+                  <div className="app-form-group buttons">
+                    <button className="app-form-button">CANCEL</button>
+                    <button className="app-form-button" onSubmit={handleSubmit}>
+                      SEND
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        
-      </div>          
+      </form>
     </div>
-    
-       
   );
 }
 
