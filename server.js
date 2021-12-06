ff2d11394fa861016c9433982461467b1ceda708
Adarsh Liju <adarsh.liju.abraham@gmail.com>
2021-12-06 17:03:25 +0530

    server updates

--- server.js
+++ server.js
@@ -2,23 +2,24 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-
+const port=5000;
 app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect("mongodb+srv://adarsh:ala@cluster0.w4nwc.mongodb.net/web-tech"); 
 const feedback = {
   firstName: String,
   lastName: String,
   email: String,
-  tel: String,
+  tel: Number,
   query: String,
 };
 const NewFeedback = mongoose.model("form", feedback);
 
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/index.html");
+
+app.get("/feedback", function (req, res) {
+  res.sendFile(__dirname + "/feedbackform.html");
 });
 
-app.post("/", function (req, res) {
+app.post("/feedback", function (req, res) {
   let newdoc = new NewFeedback({
     firstName: req.body.firstName,
     lastName: req.body.lastName,
@@ -27,9 +28,9 @@ app.post("/", function (req, res) {
     query: req.body.query,
   });
   newdoc.save();
-  res.redirect("/");
+  res.redirect("/feedback");
 });
 
-app.listen(5500, function () {
+app.listen(port, function () {
   console.log("Server is Running");
 });
